6f2b5fcf5cfbc9a981101e81c18c8029b74746f1
GitPaulo <work.paulo.santos98@gmail.com>
2025-04-25 21:06:46 +0100

    hooked up scenes

---
+++ scripts/Main.lua
@@ -0,0 +1,17 @@
+local Main = {
+	extends = Node2D,
+}
+
+function Main:_ready()
+	print("Main menu loaded")
+end
+
+function Main:_on_play_button_pressed()
+	self:get_tree():change_scene_to_file("res://scenes/LevelMap.tscn")
+end
+
+function Main:_on_settings_button_pressed()
+	self:get_tree():change_scene_to_file("res://scenes/Settings.tscn")
+end
+
+return Main
