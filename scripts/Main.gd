9c87f095aaefa4aff6ceab81a8180ad307168152
GitPaulo <work.paulo.santos98@gmail.com>
2025-04-26 19:53:15 +0100

    fun menu and music

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,11 +1,25 @@
 extends Control
 
+const CHARS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*"]
+const SPAWN_COUNT = 30
+
+@onready var floating_char_scene = preload("res://scenes/FloatingChar.tscn")
+
 func _ready() -> void:
 	SaveData.load_game()
 	print("Main menu loaded")
 
-func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/LevelMap.tscn")
+	# Spawn floating chars
+	for i in range(SPAWN_COUNT):
+		spawn_floating_char()
 
-func _on_settings_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Settings.tscn")
+func spawn_floating_char():
+	var char_instance = floating_char_scene.instantiate()
+	char_instance.text = CHARS[randi() % CHARS.size()]
+	var screen_size = get_viewport_rect().size
+	char_instance.position = Vector2(
+		randf_range(0, screen_size.x),
+		randf_range(0, screen_size.y)
+	)
+	add_child(char_instance)
+	char_instance.z_index = -1 # Make sure it's behind buttons
