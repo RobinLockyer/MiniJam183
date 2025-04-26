7bab50b0ae469c1530e1bbe29d208c0acdaab608
GitPaulo <work.paulo.santos98@gmail.com>
2025-04-26 19:06:38 +0100

    values gen and level map colours

--- scripts/LevelMap.gd
+++ scripts/LevelMap.gd
@@ -1,11 +1,10 @@
 extends Control
 
-var current_level: int = 0
 var level_buttons: Array
 var boss_levels: Array = [4, 8]
 
 func _ready():
-	current_level = SaveData.current_level
+	SaveData.load_game()
 	level_buttons = [
 		$CenterContainer/MarginContainer/GridContainer/Level1Button,
 		$CenterContainer/MarginContainer/GridContainer/Level2Button,
@@ -26,31 +25,78 @@ func _ready():
 			button.connect("pressed", Callable(self, "_on_level_pressed").bind(i))
 
 	update_level_buttons()
-
+	
 func update_level_buttons():
+	var current_level = 4
+
 	for i in range(level_buttons.size()):
 		var button = level_buttons[i]
-		var unlocked = (i <= current_level + 1) # <= current + 1
-		
+		var level_data = SaveData.levels[i]
+		var value = level_data["value"]
+		var is_boss = level_data["is_boss"]
+
+		var unlocked = (i <= current_level + 1)
+		var is_current = (i == current_level)
+		var is_next = (i == current_level + 1)
+		var is_completed = (i < current_level)
+
 		if unlocked:
-			button.disabled = false
-			button.text = button.name
-			button.modulate = Color(1, 1, 1, 1)
+			button.text = "Level %d\nValue: %d" % [i + 1, value]
+			if is_boss:
+				button.text += "\n (Boss)"
+
+			if is_completed:
+				button.modulate = Color(0.4, 1, 0.4, 1) # Green
+			elif is_current:
+				button.modulate = Color(0.4, 0.6, 1, 1) # Blue
+			elif is_next:
+				button.modulate = Color(1, 1, 1, 1) # White
+
+			# Clickable only if current
+			button.disabled = not is_current
+
+			# Cursor and tooltip
+			if is_current:
+				button.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
+				button.tooltip_text = "This is the current level!"
+			elif is_completed:
+				button.mouse_default_cursor_shape = Control.CURSOR_ARROW
+				button.tooltip_text = "Completed!"
+			elif is_next:
+				button.mouse_default_cursor_shape = Control.CURSOR_ARROW
+				button.tooltip_text = "This is the next level!"
 		else:
+			# If locked
 			button.disabled = true
 			button.text = "?"
 			button.modulate = Color(1, 1, 1, 0.5)
+			button.mouse_default_cursor_shape = Control.CURSOR_ARROW
+			button.tooltip_text = "Locked!"
+			
+		if is_boss:
+			var style = StyleBoxFlat.new()
+			style.bg_color = Color(1, 0.3, 0.3, 1)
+			style.border_color = Color(1, 0.3, 0.3) # Red border
+			style.border_width_top = 4
+			style.border_width_bottom = 4
+			style.border_width_left = 4
+			style.border_width_right = 4
+
+			button.add_theme_stylebox_override("normal", style)
+			button.add_theme_stylebox_override("hover", style)
+			button.add_theme_stylebox_override("pressed", style)
+			button.add_theme_stylebox_override("focus", style)
 
 func _on_level_pressed(level_index: int):
 	print("Clicked level:", level_index)
 	var level_scene = load("res://scenes/Level.tscn").instantiate()
 
-	# Set parameters
-	level_scene.level_index = level_index
-	level_scene.is_boss = boss_levels.has(level_index)
-	level_scene.target_number = 24 # TODO
+	var level_data = SaveData.levels[level_index]
 
-	# Change scene manually
+	level_scene.level_index = level_index
+	level_scene.is_boss = level_data["is_boss"]
+	level_scene.target_number = level_data["value"]
+
 	get_tree().root.add_child(level_scene)
 	get_tree().current_scene.queue_free()
 	get_tree().current_scene = level_scene
--- scripts/SaveData.gd
+++ scripts/SaveData.gd
@@ -3,21 +3,42 @@ extends Node
 var save_path: String = "user://save_game.cfg"
 var config: ConfigFile = ConfigFile.new()
 
-var current_level: int = 1
+var current_level: int = 0
 var points: int = 0
+var levels: Array = []
+var boss_levels: Array = [4, 8]
 
 func save_game() -> void:
 	config.set_value("game", "current_level", current_level)
 	config.set_value("game", "points", points)
+	config.set_value("game", "levels", levels)
 	config.save(save_path)
 	print("Game saved!")
 
 func load_game() -> void:
 	var err = config.load(save_path)
 	if err == OK:
-		current_level = config.get_value("game", "current_level", 1)
+		current_level = config.get_value("game", "current_level", 0)
 		points = config.get_value("game", "points", 0)
-		print("Game loaded! Level:", current_level, "Points:", points)
+		levels = config.get_value("game", "levels", [])
+
+		if levels.is_empty():
+			print("No level data found, generating...")
+			generate_levels()
+			save_game()
+		else:
+			print("Game and levels loaded!")
+
 	else:
-		print("No save file found. Creating default save...")
+		print("No save file found. Generating default save...")
+		generate_levels()
 		save_game()
+
+func generate_levels() -> void:
+	levels.clear()
+	for i in range(9):
+		var level_data = {
+			"value": 24 if i == 0 else randi_range(0, 100),
+			"is_boss": boss_levels.has(i)
+		}
+		levels.append(level_data)
