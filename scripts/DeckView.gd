69437c66354642dbeeab23894c571ed878c6601d
RobinLockyer <lockyer.ro@gmail.com>
2025-04-27 11:47:21 +0100

    Added redraw button. Cards now have minimum size

--- scripts/DeckView.gd
+++ scripts/DeckView.gd
@@ -3,18 +3,18 @@ extends Control
 class_name DeckView
 
 const deck_view_scene: Resource = preload("res://scenes/DeckView.tscn")
+const card_display_scene: Resource = preload("res://scenes/CardDisplay.tscn")
 
 var deck: Array[Card]
 
 static func open(parent: Node, cardList: Array[Card]) -> void:
-	print("open")
 	var deckview = deck_view_scene.instantiate()
 	deckview.deck = cardList
 	parent.add_child(deckview)
 
 func _enter_tree() -> void:
 	for card in deck:
-		var card_display = CardDisplay.new()
+		var card_display = card_display_scene.instantiate()
 		card_display.card = card
 		$PanelContainer/DeckDisplayContainer.add_child(card_display)
 
--- scripts/level.gd
+++ scripts/level.gd
@@ -11,6 +11,9 @@ var current: float = 0
 var draw_pile: Array[Card]
 var hand_size: int = 6
 var error_margin: int = 0
+var remaining_redraws: int = 2
+
+var card_display_scene: Resource = preload("res://scenes/CardDisplay.tscn")
 
 func _ready() -> void:
 	label_target_value.text = str(target_number)
@@ -18,10 +21,16 @@ func _ready() -> void:
 	draw_pile = SaveData.deck.duplicate()
 	draw_pile.shuffle()
 	for i in hand_size:
-		var card_display = CardDisplay.new()
-		card_display.card = draw_pile.pop_front()
-		card_display.pressed.connect(card_clicked.bind(card_display))
-		hand_container.add_child(card_display)
+		draw_card()
+		
+func draw_card() -> void:
+	var card_display: CardDisplay = card_display_scene.instantiate()
+	card_display.card = draw_pile.pop_front()
+	card_display.get_children()[0].pressed.connect(card_clicked.bind(card_display))
+	card_display.get_children()[1].pressed.connect(card_redraw.bind(card_display))
+	if remaining_redraws > 0:
+		card_display.set_redraw_visible(true)
+	hand_container.add_child(card_display)
 		
 	
 func card_clicked(card_display: CardDisplay) -> void:
@@ -37,6 +46,14 @@ func card_clicked(card_display: CardDisplay) -> void:
 		SaveData.current_level += 1
 		SaveData.save_game()
 		
+func card_redraw(card_display: CardDisplay) -> void:
+	if remaining_redraws > 0:
+		card_display.queue_free()
+		draw_card()
+		remaining_redraws -= 1
+		if remaining_redraws <= 0:
+			for display in hand_container.get_children():
+				display.set_redraw_visible(false)
 	
 func update_current_value_label():
 	label_current_value.text = str(current)
